ee4088e92e18919aecd4b7088b11d9619f64da19
Everton Vanoni <86416772+vertocode@users.noreply.github.com>
2022-04-03 19:28:19 -0300

    feat: add overlay of before start and add README

---
+++ home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <Overlay>
+    <template #header>
+      <h1>header</h1>
+    </template>
+    <template #default>
+      <h1>Default</h1>
+    </template>
+    <template #footer>
+      <h1>Footer</h1>
+    </template>
+  </Overlay>
+</template>
+
+<script lang="ts" setup>
+import Overlay from '@/components/Overlay.vue'
+
+</script>
